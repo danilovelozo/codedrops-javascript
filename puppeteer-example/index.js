5f2e1f52e11aa5b37f198ef654ac49a0d39a4a40
Danilo Velozo <velozo.dan@gmail.com>
2021-05-28 17:08:20 -0300

    Add Project

---
+++ puppeteer-example/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+// we're using async/await - so we need and async function, that we can run
+
+const run = async () => {
+  // open the browser and prepare a page
+  const browser = await puppetter.launch();
+  const page = await browser.newPage();
+  // open the page to scrape
+  await page.goto('https://codesnacks.net');
+
+  // execute the JS in the context of the page to get all the links
+
+  const links = await page.evaluate(() => {
+    // let's just get all links and create an aeeay from the resulting NodeList
+    Array.from(document.querySelectorAll("a")).map(anchor => [anchor.href, anchor.textContent])
+  });
+
+  // output all the links
+  console.log(links);
+
+  // close the browser
+  await browser.close();
+};
+
+// run the async function
+run();
